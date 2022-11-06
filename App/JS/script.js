9abf92b465f724dc760a265288117b1b8851e7da
NjimsRane <njims2011@gmail.com>
2022-11-06 18:45:30 +0100

    started with media query

--- App/JS/script.js
+++ App/JS/script.js
@@ -1,2 +1,12 @@
 const setDate = document.querySelector(".set-date");
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+
+
 setDate.innerHTML = new Date().getFullYear();
+
+
+
+hamburgerMenu.addEventListener('click',()=>{
+    hamburgerMenu.classList.toggle('hamburger-menu-display')
+})
