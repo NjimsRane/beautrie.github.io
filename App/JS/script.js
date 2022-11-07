cc91f09cd2c109a85153138afac547c665e42dc0
NjimsRane <njims2011@gmail.com>
2022-11-07 12:20:42 +0100

    added more media queries

--- App/JS/script.js
+++ App/JS/script.js
@@ -1,12 +1,14 @@
 const setDate = document.querySelector(".set-date");
 const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hero = document.querySelector('.hero');
+const menuLinks = document.querySelector('.menu-links');
 
 
 
 setDate.innerHTML = new Date().getFullYear();
 
-
-
 hamburgerMenu.addEventListener('click',()=>{
-    hamburgerMenu.classList.toggle('hamburger-menu-display')
+    hamburgerMenu.classList.toggle('hamburger-menu-display');
+    hero.classList.toggle('hero-blur');
+    menuLinks.classList.toggle('menu-links-move')
 })
