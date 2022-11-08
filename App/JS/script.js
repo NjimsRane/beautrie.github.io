7567c637d16ab20f91f0a54546a358a411abb9df
NjimsRane <njims2011@gmail.com>
2022-11-08 12:36:29 +0100

    started over and done already with the header

--- App/JS/script.js
+++ App/JS/script.js
@@ -1,14 +1,9 @@
-const setDate = document.querySelector(".set-date");
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const hero = document.querySelector('.hero');
-const menuLinks = document.querySelector('.menu-links');
-
-
-
-setDate.innerHTML = new Date().getFullYear();
+const navLinks = document.querySelector('.nav-links');
 
 hamburgerMenu.addEventListener('click',()=>{
-    hamburgerMenu.classList.toggle('hamburger-menu-display');
+    hamburgerMenu.classList.toggle('hamburger-menu-show');
     hero.classList.toggle('hero-blur');
-    menuLinks.classList.toggle('menu-links-move')
+    navLinks.classList.toggle('nav-links-show')
 })
