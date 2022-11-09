72741b4faa232da53555a5aea6f98fbec9f2ba04
NjimsRane <njims2011@gmail.com>
2022-11-09 22:15:16 +0100

    added sign up page

---
+++ App/JS/signup.js
@@ -0,0 +1,4 @@
+const btnPass = document.querySelectorAll('.btn-pass');
+const passShowOrHide = document.querySelectorAll('.img-hide-show');
+const inputPassword = document.querySelectorAll('#password');
+
