d7ce59d8dfe050c58b424eab58ae0810e094b6c7
NjimsRane <njims2011@gmail.com>
2022-11-10 13:34:08 +0100

    done with media query of the header

--- App/JS/signup.js
+++ App/JS/signup.js
@@ -1,4 +1,23 @@
 const btnPass = document.querySelectorAll('.btn-pass');
 const passShowOrHide = document.querySelectorAll('.img-hide-show');
-const inputPassword = document.querySelectorAll('#password');
+const inputPassword = document.querySelectorAll('.password');
+
+btnPass.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        showPassword();
+    })
+});
+
+function showPassword(){
+    inputPassword.forEach((pass)=>{
+        if(pass.value === ""){
+            return
+        }else if(pass.type === "password"){
+            pass.type = 'text'
+        }else{
+            pass.type="password"
+        }
+    })
+}
+
 
