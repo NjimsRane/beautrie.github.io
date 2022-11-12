4b39390f4b83d010ecb82df6d087b1dd2e023b9b
NjimsRane <njims2011@gmail.com>
2022-11-12 17:40:46 +0100

    done with password check

--- App/JS/signup.js
+++ App/JS/signup.js
@@ -1,23 +1,43 @@
-const btnPass = document.querySelectorAll('.btn-pass');
-const passShowOrHide = document.querySelectorAll('.img-hide-show');
-const inputPassword = document.querySelectorAll('.password');
+const btnPass = document.querySelector('.btn-pass');
+const btnSignup = document.querySelector('.btn-signup');
+const passShowOrHide = document.querySelector('.img-hide-show');
+const inputPassword = document.querySelector('.password');
+const verifyPassword = document.querySelector('.verify-password');
+const matchPassword = document.querySelector('.match-password');
+const underlineMatch = document.querySelector('.underline-match');
 
-btnPass.forEach((btn)=>{
-    btn.addEventListener('click',()=>{
-        showPassword();
-    })
+btnPass.addEventListener('click',function(){
+    if(inputPassword.value === ""){
+        return
+    }else if(inputPassword.type === "password"){
+        inputPassword.type = 'text';
+        passShowOrHide.src = '/Images/pass/hide.png';
+    }else{
+        inputPassword.type="password";
+        passShowOrHide.src = '/Images/pass/show.png';
+    }
 });
 
-function showPassword(){
-    inputPassword.forEach((pass)=>{
-        if(pass.value === ""){
-            return
-        }else if(pass.type === "password"){
-            pass.type = 'text'
-        }else{
-            pass.type="password"
-        }
-    })
-}
+btnSignup.addEventListener('click',function(){
+    if(verifyPassword.value != inputPassword.value){
+        underlineMatch.style.backgroundColor ='red';
+        matchPassword.textContent = 'passwords don`t match';
+        matchPassword.style.color = 'red';
+        verifyPassword.style.color = 'white';
+        
+    }else{
+        underlineMatch.style.backgroundColor = 'green';
+        matchPassword.textContent = 'correct passwords';
+        matchPassword.style.color = 'green';
+        verifyPassword.style.color = 'white';
+    }
+})
+
+
+
+
+
+
+
 
 
